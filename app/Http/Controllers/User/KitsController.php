770a7531061ef9912ad6170ae660aa100eb9b2cd
rfcm29 <r.f.c.m.29@gmail.com>
2022-07-12 16:03:27 +0100

    adicionado filtro para items eliminados

--- app/Http/Controllers/User/KitsController.php
+++ app/Http/Controllers/User/KitsController.php
@@ -17,10 +17,12 @@ class KitsController extends Controller
         }
         else{
             if(request('search')){
-                $kits = Kit::where('description', 'LIKE', '%'.request('search').'%')
-                                    ->where('kit_category_id', $id)->get();
+                $kits = Kit::where('kit_state_id', '!=', 3)
+                            ->where('description', 'LIKE', '%'.request('search').'%')
+                            ->where('kit_category_id', $id)->get();
             } else {
-                $kits = Kit::where('kit_category_id', $id)->get();
+                $kits = Kit::where('kit_state_id', '!=', 3)
+                            ->where('kit_category_id', $id)->get();
             }
         }
 
@@ -40,12 +42,12 @@ class KitsController extends Controller
     }
 
     private function filteredKits($id){
-        $kits = Kit::where('kit_category_id', $id)->doesntHave('reserves')
-                ->orWhere('kit_category_id', $id)->whereDoesntHave('reserves', function(Builder $query){
+        $kits = Kit::where('kit_state_id', '!=', 3)->where('kit_category_id', $id)->doesntHave('reserves')
+                ->orWhere('kit_state_id', '!=', 3)->where('kit_category_id', $id)->whereDoesntHave('reserves', function(Builder $query){
                     $query->whereBetween('start_date', [session()->get('reserve.start_date'), session()->get('reserve.end_date')])
                         ->orWhereBetween('end_date', [session()->get('reserve.start_date'), session()->get('reserve.end_date')]);
                 })
-                ->orWhere('kit_category_id', $id)->whereDoesntHave('reserves', function(Builder $query){
+                ->orWhere('kit_state_id', '!=', 3)->where('kit_category_id', $id)->whereDoesntHave('reserves', function(Builder $query){
                     $query->where('start_date', '>' , session()->get('reserve.end_date'))
                         ->orWhere('end_date', '<', session()->get('reserve.start_date'));
                 })
